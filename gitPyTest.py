c7a8266dd4e23f6a0179f1d8ed13da540cb69a37
YanhuaXie <xie78@wisc.edu>
2021-07-09 20:47:12 -0500

    add class definition

--- gitPyTest.py
+++ gitPyTest.py
@@ -5,5 +5,23 @@ print('gdal test under Git')
 print('github test')
 print('python-github test')
 
+x = np.linspace(0, 10, 50)
+# print(x)
 
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        # return self.name
+        print(self.name)
+
+    def __repr__(self):
+        return f"the name is {self.name}"
+
+
+person1 = Person("Yanhua")
+print(person1)
+person1.print_name()
 
